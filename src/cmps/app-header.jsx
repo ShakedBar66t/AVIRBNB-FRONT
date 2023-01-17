84d74ae6694858f83ab4309c0ffe224e6dde7558
Itai Belotserkovski <itaibelo2904@gmail.com>
2023-01-18 00:30:39 +0200

    Rebuilding the appheader

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,18 +1,24 @@
-import { Link, NavLink } from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import routes from '../routes'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { login, logout, signup } from '../store/user.actions.js'
 import { LoginSignup } from './login-signup.jsx'
+import { useState } from 'react'
 
 export function AppHeader() {
-    const user = useSelector(storeState => storeState.userModule.user)
 
+    const [userModal, setUserModal] = useState(false)
+    const [filterModal, setFilterModal] = useState(false)
+    const navigate = useNavigate()
+
+
+    const user = useSelector(storeState => storeState.userModule.user)
     async function onLogin(credentials) {
         try {
             const user = await login(credentials)
             showSuccessMsg(`Welcome: ${user.fullname}`)
-        } catch(err) {
+        } catch (err) {
             showErrorMsg('Cannot login')
         }
     }
@@ -20,7 +26,7 @@ export function AppHeader() {
         try {
             const user = await signup(credentials)
             showSuccessMsg(`Welcome new user: ${user.fullname}`)
-        } catch(err) {
+        } catch (err) {
             showErrorMsg('Cannot signup')
         }
     }
@@ -28,34 +34,72 @@ export function AppHeader() {
         try {
             await logout()
             showSuccessMsg(`Bye now`)
-        } catch(err) {
+        } catch (err) {
             showErrorMsg('Cannot logout')
         }
+    }
+
+    function toggleUserModal() {
+        setUserModal(!userModal)
+    }
+    function toggleFilterModal() {
+        setFilterModal(!filterModal)
     }
 
     return (
         <header className="app-header">
             <nav>
-                {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
-                <NavLink to='/stay'>Stays </NavLink>
+                {/* {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
+                <NavLink to='/stay'>Stays </NavLink> */}
+                <div className='logo-container'>
+                    <img className='header-logo' src={require(`../assets/img/airbnb-header-logo.png`)} alt='' onClick={() => navigate('/stay')} />
+                </div>
+                <div className='filter-container'>
+                    <div className='filter-btns' onClick={toggleFilterModal}>
+                        <button className='location-filter header-btn'>Anywhere |</button>
+                        <button className='time-filter header-btn'>Any week |</button>
+                        <button className='guest-filter header-btn'>Add guests </button>
+                        <button className='search-btn  user-header-btn'>🔍</button>
+                    </div>
+                </div>
+                <div className='user-nav-container'>
+                    <button className='host-btn user-header-btn'>Airbnb your home</button>
+                    <button className='lang-btn  user-header-btn'>🌐</button>
+                    <button onClick={toggleUserModal} className='user-info-btn header-btn' ><span>=</span><span>👤</span></button>
+                </div>
 
-                {user &&
+                {/* {user &&
                     <span className="user-info">
                         <Link to={`user/${user._id}`}>
-                            {user.imgUrl && <img src={user.imgUrl} />}
-                            {user.fullname}
+                        {user.imgUrl && <img src={user.imgUrl} />}
+                        {user.fullname}
                         </Link>
                         <span className="score">{user.score?.toLocaleString()}</span>
                         <button onClick={onLogout}>Logout</button>
-                    </span>
-                }
-                {!user &&
-                    <section className="user-info">
+                        </span>
+                    }
+                    {!user &&
+                        <section className="user-info">
                         <LoginSignup onLogin={onLogin} onSignup={onSignup} />
-                    </section>
-                }
+                        </section>
+                    } */}
             </nav>
-            <h1>My App</h1>
+            <div className={`user-modal ${userModal ? 'open' : ''}`}>
+                <button>Log in </button>
+                <button>Sign up </button>
+                <button>Airbnb your home </button>
+                <button>Host an experience </button>
+                <button>Help </button>
+            </div>
+            <div className={`filter-modal ${filterModal ? 'open' : ''}`}>
+                <button>where </button>
+                <button>Check in </button>
+                <button>Check out </button>
+                <button>Who </button>
+                <button className='search-btn  user-header-btn'>🔍</button>
+            </div>
+            <div className='labels-container'>
+            </div>
         </header>
     )
 }
