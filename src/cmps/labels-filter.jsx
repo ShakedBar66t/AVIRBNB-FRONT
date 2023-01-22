0bf9dcc8a4e781bef50ca974afffa3928471c6c4
Itai Belotserkovski <itaibelo2904@gmail.com>
2023-01-22 23:57:15 +0200

    added header to details

--- src/cmps/labels-filter.jsx
+++ src/cmps/labels-filter.jsx
@@ -61,10 +61,9 @@ export function LabelsFilter() {
                     })
                 }
             </div>
-            {(currentIndex) && <div className={`'btn-container prev'`}>
-                <button className={`prev-btn  ${!currentIndex ? 'hidden' : ''}`} onClick={handlePrev}> <BiChevronLeft /></button>
+            <div className='btn-container prev '>
+                <button className={`prev-btn ${!currentIndex ? 'hidden' : ''}`} onClick={handlePrev}> <BiChevronLeft /></button>
             </div>
-            }
             <div className='btn-container next'>
                 <button className={`next-btn ${(currentIndex >= stayLabels.length - displayCount * 3) ? 'hidden' : ''}`} onClick={handleNext}> <BiChevronRight /></button>
             </div>
