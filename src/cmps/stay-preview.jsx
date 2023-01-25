bf0ac3c60f78d307ee2c11c504a97e54a4ea8b35
YonatanFinerman <julianmode808@gmail.com>
2023-01-25 11:15:02 +0200

    a

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -87,7 +87,6 @@ export function StayPreview({ stay,onToggleLike }) {
             <p className="prev-host-name">{"Hosted by " + (stay.host.fullname.slice(0,stay.host.fullname.indexOf(' ')))}</p>
             <PrevDateStr />
             <p className="prev-price">{'$'+ stay.price + ' night'}</p>
-
         </div>      
     </article>
 }
